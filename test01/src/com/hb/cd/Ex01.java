f74d7a4df70dad4d99d70f1ab5953a8f05c42889
tk0720 <tk0720@naver.com>
2017-11-14 17:53:04 +0900

    test01

---
+++ test01/src/com/hb/cd/Ex01.java
@@ -0,0 +1,15 @@
+package com.hb.cd;
+
+public class Ex01 {
+	public int sabun;
+	protected String name;
+	java.util.Date nalja;
+	private int pay;
+	
+	public void func01() {}
+	
+	protected int func02() {
+		return 0;
+	}
+
+}
